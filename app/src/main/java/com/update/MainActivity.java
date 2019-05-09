a6e3aea61c0becc2adf5c0eb70ca032ff7a08132
liupu <575866447@qq.com>
2019-05-09 11:22:52 +0800

    bugfix

--- app/src/main/java/com/update/MainActivity.java
+++ app/src/main/java/com/update/MainActivity.java
@@ -3,7 +3,6 @@ package com.update;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.update.local.R;
 
 public class MainActivity extends AppCompatActivity {
 
